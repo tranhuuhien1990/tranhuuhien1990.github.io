6e20689b929928da9da4d17a58b07d1c0cc58c12
withtalktest1 <107847534+withtalktest1@users.noreply.github.com>
2023-11-09 16:20:26 +0700

    init

--- src/components/Square.js
+++
@@ -1,9 +0,0 @@
-function Square({ value, onSquareClick }) {
-    return (
-        <button className="square" onClick={onSquareClick}>
-            {value}
-        </button>
-    );
-}
-
-export default Square;
